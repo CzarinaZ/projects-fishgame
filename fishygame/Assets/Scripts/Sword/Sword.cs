02690b9546e5c33a185ba4bbb07f3ecb58083826
CzarinaZ <barengczarina31@gmail.com>
2025-12-18 14:01:58 -0800

    added Sword Collision

--- fishygame/Assets/Scripts/Sword/Sword.cs
+++ fishygame/Assets/Scripts/Sword/Sword.cs
@@ -6,6 +6,8 @@ public class Sword : MonoBehaviour
 {
     [SerializeField] private GameObject slashAnimPrefab;
     [SerializeField] private Transform slashAnimSpawnPoint;
+    [SerializeField] private Transform weaponCollider;
+
     private PlayerControls playerControls;
     private Animator myAnimator;
     private PlayerController playerController;
@@ -34,11 +36,16 @@ public class Sword : MonoBehaviour
     private void Attack()
     {
         myAnimator.SetTrigger("Attack");
+        weaponCollider.gameObject.SetActive(true);
 
         slashAnim = Instantiate(slashAnimPrefab, slashAnimSpawnPoint.position, Quaternion.identity);
         slashAnim.transform.parent = this.transform.parent;
     }
-    public void SwingUpFlipAnim()
+    public void DoneAttackingAnimEvent()
+    {
+        weaponCollider.gameObject.SetActive(false);
+    }
+    public void SwingUpFlipAnimEvent()
     {
         slashAnim.gameObject.transform.rotation = Quaternion.Euler(-180, 0, 0);
 
@@ -49,7 +56,7 @@ public class Sword : MonoBehaviour
         }
         */
     }
-    public void SwingDownFlipAnim()
+    public void SwingDownFlipAnimEvent()
     {
         slashAnim.gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);
         //add when I do animations
@@ -70,10 +77,12 @@ public class Sword : MonoBehaviour
         if(mousePos.x <playerScreenPoint.x)
         {
             activeWeapon.transform.rotation = Quaternion.Euler(0, 180, -angle);
+            weaponCollider.transform.rotation = Quaternion.Euler(0,-180,0);
         }
         else
         {
             activeWeapon.transform.rotation = Quaternion.Euler(0, 0, angle);
+            weaponCollider.transform.rotation = Quaternion.Euler(0,0,0);
         }
     }
 }
