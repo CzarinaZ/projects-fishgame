42aa41a17866a57ec0c20e79a458aabc43e49504
CzarinaZ <barengczarina31@gmail.com>
2026-01-12 15:08:16 -0800

    Updated the DamageSource and EnemyHealth scripts. Removed Flash and Knockback scripts

--- fishygame/Assets/Scripts/Enemy/EnemyHealth.cs
+++ fishygame/Assets/Scripts/Enemy/EnemyHealth.cs
@@ -7,14 +7,6 @@ public class EnemyHealth : MonoBehaviour
     [SerializeField] private int startingHealth = 3;
 
     private int currentHealth;
-    private Knockback knockback;
-    private Flash flash;
-
-    private void Awake()
-    {
-        flash = Get.Component<Flash>();
-        knockback = Get.Component<Knockback>();
-    }
 
     private void Start()
     {
@@ -25,15 +17,12 @@ public class EnemyHealth : MonoBehaviour
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
-        knockback.GetKnockedBack(PlayerController.Instance.transform, 15f);
-        StartCoroutine(flash.FlashRoutine());
+        Debug.Log(currentHealth);
+        DetectDeath();
     }
 
-    //Removes the entity when health drops to 0
-    public void DetectDeath()
-    {
-        if(currentHealth <= 0)
-        {
+    private void DetectDeath(){
+        if (currentHealth <= 0){
             Destroy(gameObject);
         }
     }
--- fishygame/Assets/Scripts/Sword/Sword.cs
+++ fishygame/Assets/Scripts/Sword/Sword.cs
@@ -1,105 +1,82 @@
-using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
 
 public class Sword : MonoBehaviour
 {
     [SerializeField] private GameObject slashAnimPrefab;
     [SerializeField] private Transform slashAnimSpawnPoint;
-
-    //Sets the weapon collider as a block
     [SerializeField] private Transform weaponCollider;
 
     private PlayerControls playerControls;
-
-    //Animation Variables
     private Animator myAnimator;
     private PlayerController playerController;
     private ActiveWeapon activeWeapon;
+
     private GameObject slashAnim;
 
-
-    private void Awake()
-    {
+    private void Awake() {
+        playerController = GetComponentInParent<PlayerController>();
+        activeWeapon = GetComponentInParent<ActiveWeapon>();
         myAnimator = GetComponent<Animator>();
         playerControls = new PlayerControls();
-        activeWeapon = GetComponentInParent<activeWeapon>();
-        playerController = GetComponentInParent<PlayerController>();
     }
-    private void OnEnable()
-    {
+
+    private void OnEnable() {
         playerControls.Enable();
     }
+
     void Start()
     {
         playerControls.Combat.Attack.started += _ => Attack();
     }
-    private void Update()
-    {
+
+    private void Update() {
         MouseFollowWithOffset();
     }
 
-    //Code for Attack
-    private void Attack()
-    {
-        //Triggered when mouse is clicked
+    private void Attack() {
         myAnimator.SetTrigger("Attack");
-
-        //sets the weapon collider to be true
         weaponCollider.gameObject.SetActive(true);
 
-        //Slash Animation
         slashAnim = Instantiate(slashAnimPrefab, slashAnimSpawnPoint.position, Quaternion.identity);
         slashAnim.transform.parent = this.transform.parent;
     }
 
-    //Code when the player attacking is done
-    public void DoneAttackingAnimEvent()
-    {
+    public void DoneAttackingAnimEvent() {
         weaponCollider.gameObject.SetActive(false);
     }
 
-    //Animation Swinging up
-    public void SwingUpFlipAnimEvent()
-    {
+
+    public void SwingUpFlipAnimEvent() {
         slashAnim.gameObject.transform.rotation = Quaternion.Euler(-180, 0, 0);
 
-// Add when I do animations
-        /*if(playerController.FacingLeft)
-        {
+        if (playerController.FacingLeft) { 
             slashAnim.GetComponent<SpriteRenderer>().flipX = true;
         }
-        */
     }
 
-    //Animation swinging down
-    public void SwingDownFlipAnimEvent()
-    {
+    public void SwingDownFlipAnimEvent() {
         slashAnim.gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);
-        //add when I do animations
-        /*
+
         if (playerController.FacingLeft)
         {
             slashAnim.GetComponent<SpriteRenderer>().flipX = true;
-        } 
-        */
+        }
     }
-    private void MouseFollowWithOffset()
-    {
+
+    private void MouseFollowWithOffset() {
         Vector3 mousePos = Input.mousePosition;
         Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(playerController.transform.position);
 
-        float angle = Mathf.Atan2(mousePos.x, mousePos.y) * Mathf.Rad2Deg;
+        float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
 
-        if(mousePos.x <playerScreenPoint.x)
-        {
-            activeWeapon.transform.rotation = Quaternion.Euler(0, 180, -angle);
-            weaponCollider.transform.rotation = Quaternion.Euler(0,-180,0);
-        }
-        else
-        {
+        if (mousePos.x < playerScreenPoint.x) {
+            activeWeapon.transform.rotation = Quaternion.Euler(0, -180, angle);
+            weaponCollider.transform.rotation = Quaternion.Euler(0, -180, 0);
+        } else {
             activeWeapon.transform.rotation = Quaternion.Euler(0, 0, angle);
-            weaponCollider.transform.rotation = Quaternion.Euler(0,0,0);
+            weaponCollider.transform.rotation = Quaternion.Euler(0, 0, 0);
         }
     }
 }
